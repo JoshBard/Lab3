0b2894ef154f7b31e133e1107c263eec1cac8cc7
Bardwick <jbardwic@signals43.bu.edu>
2022-09-30 11:36:49 -0400

    Initial errors were just syntax problems. nut later realized that ladder prompt needed to repeat until a negative number was inputted so had to fix that.

---
+++ Lab3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int limit, count = 0;
+  do {
+  cout << "How tall of a ladder do you need? ";
+  cin >> limit;
+  if (limit == 0){
+    cout << "The height can't be zero" << endl;}
+  if (limit < 0){
+      cout << "That is invalid" << endl;
+      return 0;}}
+  while (limit <= 0);
+  while (count < limit){
+    if(count%2){
+      cout << "######"  << endl;}
+    else{
+      cout << "#  #" << endl;}
+    count += 1;
+  }
+  return 0;
+  }
